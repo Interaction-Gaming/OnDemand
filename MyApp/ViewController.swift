9a2faaf7dac83d52b92e9ebeebf4681ab08ac0d1
oritn <orit.neumann@gmail.com>
2019-12-09 14:31:32 +0200

    adding some changes

--- MyApp/ViewController.swift
+++ MyApp/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, UIWe
         super.loadView()
         myWebView = UIWebView()
         myWebView.delegate = self
+        HTTPCookieStorage.shared.cookieAcceptPolicy = .always
        // myWebView.uiDelegate = self
         
 
@@ -29,20 +30,13 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, UIWe
         super.viewDidLoad()
         
         do{
-            
-            
             guard let htmlPath = Bundle.main.path(forResource: "BaseGame", ofType: "html")
                 else{
                     return
             }
-             print("htmlPath: " + htmlPath)
             let url = URL(fileURLWithPath: htmlPath)
             let content = try String(contentsOfFile: htmlPath)
             myWebView.loadHTMLString(content as String, baseURL: url)
-            
-            let libraryPath = try! FileManager.default.url(for: .libraryDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-             print("libraryPath2: " + libraryPath.absoluteString)
-           // myWebView.stringByEvaluatingJavaScript(from: "updateLibraryPath(\(libraryPath))")
             
         }
         catch{
@@ -54,9 +48,9 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, UIWe
     }
     func webViewDidFinishLoad(_ webView: UIWebView) {
         let libraryPath = try! FileManager.default.url(for: .libraryDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-        print("libraryPath1: " + libraryPath.absoluteString)
-     //    let status = myWebView.stringByEvaluatingJavaScript(from: "updateLibraryPath(\(libraryPath))")
-       // print(status)
+       
+         myWebView.stringByEvaluatingJavaScript(from: "updateLibraryPath('\(libraryPath.absoluteString)')")
+ 
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -73,17 +67,18 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, UIWe
             else{
                 self.accessResources()
             }*/
-       //  let libraryPath = try! FileManager.default.url(for: .libraryDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-      //  let scripsLocation = Bundle.main.url(forResource: "secure", withExtension: "")!
-       // let scripsLocation = URL.init(fileURLWithPath: scripsStr)
-      //  let scriptsDestination = libraryPath.appendingPathComponent("secure")
-      //  do {
-      //  try FileManager.default.moveItem(at: scripsLocation, to: scriptsDestination)
-      //  }
-       // catch
-       // {
+        //MOVE SCRIPTS TO LIBRARY FOLDER
+        
+        /*let libraryPath = try! FileManager.default.url(for: .libraryDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        let scripsLocation = Bundle.main.url(forResource: "secure", withExtension: "")!
+        let scriptsDestination = libraryPath.appendingPathComponent("secure")
+        do {
+            try FileManager.default.moveItem(at: scripsLocation, to: scriptsDestination)
+        }
+        catch
+        {
             
-      //  }
+        }*/
     }
     
     func downloadZipResource()
@@ -96,72 +91,36 @@ class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate, UIWe
                 .appendingPathComponent("secure/HTML5Games/1121/game")
                 .appendingPathComponent(zipUrl.lastPathComponent).appendingPathExtension("zip")
       
-            //print(destination)
-            
             var urlRequest = URLRequest.init(url: zipUrl)
             urlRequest.httpMethod = "get"
             urlRequest.setValue("application/zip", forHTTPHeaderField: "content-Type")
             
             // check if it exists before downloading it
         
-                //  if the file doesn't exist just download the data from your url
-                URLSession.shared.downloadTask(with: urlRequest, completionHandler: { (location, response, error) in
-                    // after downloading your data you need to save it to your destination url
-                    guard
-                        let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                        let mimeType = response?.mimeType,
-                        let location = location, error == nil
-                        else { return }
-                    do {
-                        print(destination)
-                        print(location)
-                        
-                        try FileManager.default.moveItem(at: location, to: destination)
-                        print("file saved")
-                        print("Mime Type:- \(mimeType)")
-                        
-                    } catch {
-                        print(error.localizedDescription)
-                    }
-                }).resume()
+            //  if the file doesn't exist just download the data from your url
+            URLSession.shared.downloadTask(with: urlRequest, completionHandler: { (location, response, error) in
+                // after downloading your data you need to save it to your destination url
+                guard
+                    let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                    let mimeType = response?.mimeType,
+                    let location = location, error == nil
+                    else { return }
+                do {
+                    print(destination)
+                    print(location)
+                    
+                    try FileManager.default.moveItem(at: location, to: destination)
+                    print("file saved")
+                    print("Mime Type:- \(mimeType)")
+                    
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }).resume()
         
         
     }
     
-    func getResource(){
-        
-        // create your document folder url
-        let zipUrl = try! FileManager.default.url(for: .libraryDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-        // your destination file url
-        let destination = zipUrl.appendingPathComponent(zipUrl.lastPathComponent)
-        let session = URLSession.shared
-        let url = URL(string: "https://www.googleapis.com/drive/v3/files/1wLQRvwVfDTg_EDdb6JCdfnRzLZnShiQJ?alt=media&key=AIzaSyDLCBjTx5J6Zy5n7ia38JAXO09u5-xdW0A")!
-        
-        
-        let task = session.dataTask(with: url) { data, response, error in
-            
-            if error != nil || data == nil {
-                print("Client error!")
-                return
-            }
-            
-            guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
-                print("Server error!")
-                return
-            }
-            
-           print(response)
-            
-            do {
-                let json = try JSONSerialization.jsonObject(with: data!, options: [])
-                print(json)
-            } catch {
-                print("JSON error: \(error.localizedDescription)")
-            }
-        }
-        
-        task.resume()
-    }
     
     func accessResources(){
         
