5e289d5c94a682d466c516842e506b1898a2811f
oritn <orit.neumann@gmail.com>
2019-12-23 16:08:47 +0200

    commit

--- MyApp/New Group/HTML5Games/1121/game/1280x720/Assets/BigWinPopup/res_BigWinPopup_Loc.js
+++
@@ -1,2 +0,0 @@
-var loc = loc || {}; 
- loc.res_BigWinPopup_Loc={BigWinPopup_mc:{PopupContent_mc:{TextAmountAnim_mc:{tf:{w:463.95,h:115.7,x:-19.95,y:-18.2}},TextAnim1_mc:{},TextAnim_mc:{},ParticlesContainer_mc:{}}}};
